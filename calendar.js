46abc2d399e45787266da2f2e63da4063203ca94
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2018-10-30 11:08:35 -0600

    home page style

--- calendar.js
+++ calendar.js
@@ -1,49 +1,47 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('here')
+
+// var calendar = module.exports {
+
+  function buildCalculator() {
+
+      console.log('here')
+
+      let canvas = document.getElementById('canvas')
+
+      for (let i = 0; i < 5; i++) {
+        let row = document.createElement('div')
+        row.className = 'row'
+        for (let i = 0; i < 7; i++) {
+          let pixel = document.createElement('div')
+          pixel.className = 'pixel'
+
+          row.appendChild(pixel)
+
+        }
+        canvas.appendChild(row)
+      }
 
 
+      let changeColor = (event) => {
+        console.log(event.target);
+        if (event.target.className === 'pixel') {
+          event.target.style.backgroundColor = currentColor.style.backgroundColor
+        }
+      }
 
+      // canvas.addEventListener('click', changeColor)
 
-  let canvas = document.getElementById('canvas')
+      // blue.addEventListener('click', function() {
+      //   currentColor.style.backgroundColor = 'blue'
+      // })
+      // red.addEventListener('click', function() {
+      //   currentColor.style.backgroundColor = 'red'
+      // })
+      // yellow.addEventListener('click', function() {
+      //   currentColor.style.backgroundColor = 'yellow'
+      // })
+      // green.addEventListener('click', function() {
+      //   currentColor.style.backgroundColor = 'green'
+      // })
 
-for (let i = 0; i < 5; i++) {
-  let row = document.createElement('div')
-    row.className = 'row'
-    for (let i = 0; i < 7; i++) {
-      let pixel = document.createElement('div')
-      pixel.className = 'pixel'
-
-      row.appendChild(pixel)
-
-    }
-  canvas.appendChild(row)
-}
-
-
-  let changeColor = (event) => {
-    console.log(event.target);
-    if (event.target.className === 'pixel') {
-    event.target.style.backgroundColor = currentColor.style.backgroundColor
-    }
   }
-
-  // canvas.addEventListener('click', changeColor)
-
-
-
-  // blue.addEventListener('click', function() {
-  //   currentColor.style.backgroundColor = 'blue'
-  // })
-  // red.addEventListener('click', function() {
-  //   currentColor.style.backgroundColor = 'red'
-  // })
-  // yellow.addEventListener('click', function() {
-  //   currentColor.style.backgroundColor = 'yellow'
-  // })
-  // green.addEventListener('click', function() {
-  //   currentColor.style.backgroundColor = 'green'
-  // })
-
-
-
-})
+// }
--- home.js
+++ home.js
@@ -9,7 +9,7 @@ let today = new Date()
 let userName = localStorage.getItem('User Name')
 console.log("name:",userName);
 let welcome = document.getElementById('welcome')
-welcome.innerText = `Good morning ${userName}! Please input your daily entry.`
+welcome.innerText = `Good Morning, ${userName}.`
 let currentDate = document.getElementById('currentDate')
 currentDate.innerText = `${today.getMonth() + 1}/${today.getDate()}`
 
@@ -36,6 +36,7 @@ function postEntry (event) {
     .then((response) => {
 
       console.log(response)
+      //remake calendar with new data? here with buildCalendar()
     })
     .catch((error) => {
       console.log(error)
